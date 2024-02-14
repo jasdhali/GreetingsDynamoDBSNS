934693d01d6e745abbc2bba39c41cf323e8f71ef
Jaspal <jasdhali@gmail.com>
2024-02-14 14:40:48 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Creation Date - 2/13/2024
+AWS link - https://github.com/awsdocs/aws-doc-sdk-examples/tree/master/javav2/usecases/creating_first_project
+Deployed and tested on AWS Elastic Beanstalk
---
+++ src/test/java/com/example/aws/dynamodb/GreetingsDynamoDbsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsDynamoDbsnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
